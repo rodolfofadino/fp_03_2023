d5a069d6b0efe37eabba87a04126a16c6caeff56
rodolfofadino <rodolfo.fadino@gmail.com>
2023-10-09 19:06:42 -0300

    final aula 3

--- src/fiap.api/fiap.api/Controllers/InstrumentosController.cs
+++ src/fiap.api/fiap.api/Controllers/InstrumentosController.cs
@@ -1,13 +1,16 @@
 ﻿using fiap.core.Contexts;
 using fiap.core.Models;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace fiap.api.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    [Route("api/v{api.version}/[controller]")]
     [ApiController]
+    [EnableCors("default")]
     public class InstrumentosController : Controller
     {
         private InstrumentosContext _context;
@@ -16,7 +19,6 @@ namespace fiap.api.Controllers
         {
             _context = context;
         }
-
 
         ////tipo especifico
         //[HttpGet]
@@ -38,11 +40,22 @@ namespace fiap.api.Controllers
         //    return Ok( result);
         //}
 
+        [HttpGet("")]
         public async Task<ActionResult<List<Instrumento>>> Get()
         {
             //return NotFound();
 
-            var result = await _context.Instrumentos.ToListAsync();
+            var result = await _context.Instrumentos.Where(a => a.Removed == false).ToListAsync();
+            return result;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Instrumento>> Get(int Id)
+        {
+            var result = await _context.Instrumentos.FirstOrDefaultAsync(a => a.Id == Id && a.Removed == false);
+            if (result == null)
+                return NotFound();
+
             return result;
         }
 
@@ -70,5 +83,60 @@ namespace fiap.api.Controllers
             return Created($"/api/instrumentos/{model.Id}", model);
         }
 
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Instrumento>> Update(int id, Instrumento model)
+        {
+            _context.Attach(model);
+            await _context.SaveChangesAsync();
+            return model;
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<Instrumento>> PartialUpdate(int id, Instrumento model)
+        {
+            var dbModel = await _context.Instrumentos.FirstOrDefaultAsync(a => a.Id == id);
+            if (model.Nome != dbModel.Nome)
+            {
+                dbModel.Nome = model.Nome;
+            }
+            await _context.SaveChangesAsync();
+            return model;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        //[HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            //select top 2 => se tiver mais 1 result ele da throw exception, se tiver menos de 1 result ele da throw de uma exception
+            var instrumento = await _context.Instrumentos.SingleAsync(a =>a.Id == id);
+
+           //select top 2 => se tiver mais de 1 elemento ele da throw, se tiver menos de 1 result ele retorna null (sem dar throw)
+            var instrumento2 = await _context.Instrumentos.SingleOrDefaultAsync(a => a.Id==id);
+
+            //select top 1 => se nao tiver nenhum result ele da throw de uma ex
+            var instrumento3 = await _context.Instrumentos.FirstAsync(a => a.Id == id);
+
+            //select top 1 => se nao tiver nenhum result ele retorna null
+            var instrumento4 = await _context.Instrumentos.FirstOrDefaultAsync(a => a.Id == id);
+
+
+            if (instrumento == null)
+                return NotFound();
+
+            // _context.Instrumentos.Remove(instrumento);
+            instrumento.Removed = true;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
     }
+
+
+
+
+   
 }
--- src/fiap.api/fiap.api/Program.cs
+++ src/fiap.api/fiap.api/Program.cs
@@ -10,11 +10,46 @@ var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<InstrumentosContext>
     (options => options.UseSqlServer(connection));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(config => {
+    config.RespectBrowserAcceptHeader = true;
+}).AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddCors(x => {
+    x.AddPolicy("default", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+
+    x.AddPolicy("restrita", builder =>
+    {
+        builder.WithOrigins( "api.meudominio.com.br,www.meudominio.com.br".Split(",")) ;
+    });
+});
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "fiap.api", Version = "v1" });
+});
+
+builder.Services.Configure<RouteOptions>(opt => opt.LowercaseUrls = true);
+
+
+
 
 var app = builder.Build();
 
+
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "fiap.api v1"));
+
 app.UseRouting();
+
+//app.UseCors(config => config.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseCors("restrita");
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
